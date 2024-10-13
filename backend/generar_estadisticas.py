0a9af34782c16196a5f56a70040dad92987b3ced
a23salassass <a23salassass@inspedralbes.cat>
2024-10-13 21:11:31 +0200

    PROJECTE

--- backend/generar_estadisticas.py
+++ backend/generar_estadisticas.py
@@ -1,33 +1,52 @@
 import json
 import matplotlib.pyplot as plt
+import numpy as np
+plt.style.use('classic')
 
+# Cargar el archivo JSON
 data_path = "./estadisticas.json"
 
 with open(data_path, 'r') as f:
     data = json.load(f)
 
-def obtener_estadisticas(data):
-    correctas = 0
-    incorrectas = 0
+def estadisticas_pregunta(data):
+    estadisticas = {}
 
     for partida in data["partidas"]:
         for pregunta in partida["preguntas"]:
-            if pregunta["correct"]:
-                correctas += 1
+            question_id = pregunta["questionId"]
+            correct = pregunta["correct"]
+
+            if question_id not in estadisticas:
+                estadisticas[question_id] = {"correctes": 0, "incorrectes": 0}
+
+            if correct:
+                estadisticas[question_id]["correctes"] += 1
             else:
-                incorrectas += 1
+                estadisticas[question_id]["incorrectes"] += 1
 
-    return correctas, incorrectas
+    return estadisticas
 
-# Obtener las estadísticas
-correctas, incorrectas = obtener_estadisticas(data)
+estadisticas = estadisticas_pregunta(data)
 
-# Crear gráfico de quesitos
-labels = 'Correctas', 'Incorrectas'
-sizes = [correctas, incorrectas]
-colors = ['#66b3ff', '#ff6666']
+preguntas = list(estadisticas.keys())
+correctes = [estadisticas[q]["correctes"] for q in preguntas]
+incorrectes = [estadisticas[q]["incorrectes"] for q in preguntas]
 
-plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
-plt.axis('equal')  # Asegurar que el gráfico sea circular
-plt.title("Porcentaje de Respuestas Correctas e Incorrectas")
-plt.savefig("public/estadisticas.png")  # Guarda la imagen en la carpeta 'public'
+x = np.arange(len(preguntas))
+width = 0.35
+
+fig, ax = plt.subplots()
+bars_correctes = ax.bar(x - width/2, correctes, width, label='correctes', color='g')
+bars_incorrectes = ax.bar(x + width/2, incorrectes, width, label='incorrectes', color='r')
+
+ax.set_xlabel('ID de Pregunta')
+ax.set_ylabel('Quantitat')
+ax.set_title('Respostes correctes e incorrectes per Pregunta')
+ax.set_xticks(x)
+ax.set_xticklabels(preguntas)
+ax.legend()
+
+plt.show()
+
+plt.close()
